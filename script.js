e067452f35e62430dd36baab03c64468e7c861f2
shahidul-islam-shahin <shahinshahidulislam7@gmail.com>
2024-03-19 21:28:50 +0600

    js part 7

---
+++ script.js
@@ -0,0 +1,14 @@
+let now = 2024;
+let shahinAge = 18;
+let fullAge = 21;
+let islamAge = 30;
+
+let isFullAge = now - shahinAge;
+let average = (shahinAge + islamAge) /2;
+
+
+document.getElementById('html').innerHTML = average;
+
+console.log(isFullAge)
+
+// () {} [] / * - + =
